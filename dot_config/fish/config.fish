28cf91526b3a0fc3034e6ac3df904b67dd005bc5
pd <pd@mini.local>
2023-08-11 10:38:33 +0530

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 starship init fish | source
 zoxide init fish | source
+set -x EDITOR hx
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 end
 for file in ~/.config/fish/aliases/*.fish
